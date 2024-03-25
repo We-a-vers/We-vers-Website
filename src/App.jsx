19996bfe439e6a4f5b099f3e1af2f0bbb17039b6
joannePon22 <jpon@ucsd.edu>
2024-03-24 17:53:49 -0700

    update App.js with section

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,16 @@
-import Navbar from "./Components/navbar"
+import Navbar from "./Components/navbar";
+import LandingPage from "./pages/Landing/landing_page";
 
 function App() {
   return (
     // <>test</>
     <div>
       <Navbar />
-      <div id="dummy">
-        <ul className="flex flex-col">
-          <li className="flex justify-center align-center h-[700px]" id="home">
-            Home
-          </li>
-          <li
-            className="flex justify-center align-center h-[700px]"
-            id="projects"
-          >
-            Projects
-          </li>
-          <li
-            className="flex justify-center align-center h-[700px]"
-            id="services"
-          >
-            Services
-          </li>
-          <li
-            className="flex justify-center align-center h-[700px]"
-            id="questions"
-          >
-            FAQs
-          </li>
-          <li className="flex justify-center align-center h-[700px]" id="team">
-            Our Team
-          </li>
-          <li
-            className="flex justify-center align-center h-[700px]"
-            id="contact"
-          >
-            Contact Us
-          </li>
-        </ul>
-      </div>
+      <section id = 'home'><LandingPage/></section>
+      {/* <section id = 'home'><LandingPage/></section>
+      <section id = 'home'><LandingPage/></section>
+      <section id = 'home'><LandingPage/></section>
+      <section id = 'home'><LandingPage/></section> */}
     </div>
   );
 }
