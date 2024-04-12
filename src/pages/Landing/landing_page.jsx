b3f447bd519f983332de944f948d12f40098be47
joannePon22 <jpon@ucsd.edu>
2024-04-11 21:36:09 -0700

    add tablet version

--- src/pages/Landing/landing_page.jsx
+++ src/pages/Landing/landing_page.jsx
@@ -5,20 +5,20 @@ const LandingPage = () => {
         alert('Button clicked');
       };
     return (
-    <main className="min-w-dvh flex justify-center items-center h-full">
-      <section className="w-[218.33px] desktop:w-[800px] flex flex-col font-bold font-rubik">
-          <h2 className="text-[22px] desktop:text-[82px] text-center text-black"> 
+    <main className="px-10 py-5 tablet:px-20 desktop:px-32 desktop:py-20 min-w-dvh flex justify-center items-center h-full bg-landingBG bg-no-repeat bg-center bg-cover" >
+      <section className="px-10 tablet:px-20 desktop:px-32 flex flex-col font-bold font-rubik">
+          <h2 className="text-[22px] tablet:text-[50px] desktop:text-[70px] text-center text-black"> 
           We craft 
           <span className='bg-gradient-to-r from-blue-300 to-purple-500 text-transparent bg-clip-text'> immersive </span> 
           digital experiences that resonate with 
           <span className='bg-gradient-to-r from-blue-300 to-purple-500 text-transparent bg-clip-text'> your vision</span>.
         </h2>
-        <p className="text-[10px] desktop:text-[23px] font-normal text-center font-inter my-4 desktop:my-16">
+        <p className="text-[10px] tablet:text-[15px] desktop:text-[20px] font-normal text-center font-inter my-4 desktop:my-16">
             Whether you're a business owner looking to establish your online  presence or an individual with a creative idea, we're here to turn your dreams into captivating realities.
         </p>
         <button
             onClick={handleClick}
-            className="px-8 py-2 bg-[#1596cb] hover:bg-blue-700 text-white text-[7px] desktop:text-xl font-normal font-inter mx-auto rounded"> 
+            className="px-8 py-2 bg-[#1596cb] hover:bg-blue-700 text-white text-[7px] tablet:text-[15px] desktop:text-l font-normal font-inter mx-auto rounded"> 
             Request a Quote
         </button>
     </section>
