d60dde07337dc8a46c724d9ded75206b5b4ec4a6
joannePon22 <jpon@ucsd.edu>
2024-04-11 21:36:09 -0700

    add tablet version

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,13 @@ export default {
         inter:["Inter", 'sans-serif'],
         pt:["PT Serif", 'serif']
       },
+      backgroundImage: {
+        'landingBG': "url('/landingBG.png')",
+        'landingBGS': "url('/landingBGS.png')"
+      },
       screens: {
-        desktop: '1440px',
+        desktop: '1280px',
+        tablet:'768px',
         mobile: '360px',
       },
       colors: {
