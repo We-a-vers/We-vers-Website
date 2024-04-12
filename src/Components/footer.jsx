8b5c79a2111a41cd9f9c372042429c13cf4cd161
Takkunkuma <102077650+Takkunkuma@users.noreply.github.com>
2024-04-11 21:02:40 -0700

    change the flex div to grid div and change code readability

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -1,28 +1,25 @@
 /* eslint-disable react/jsx-props-no-spreading */
 
 function Footer() {
-  const firstCol = [
+  const footitems = [
     { href: '#home', title: 'Home' },
-    { href: '#services', title: 'Services' },
-    { href: '#team', title: 'Our Team' },
-  ];
-
-  const secondCol = [
     { href: '#projects', title: 'Projects' },
+    { href: '#services', title: 'Services' },
     { href: '#questions', title: 'FAQs' },
+    { href: '#team', title: 'Our Team' },
     { href: '#contact', title: 'Contact Us' },
   ];
 
   const FootItem = ({ href, title }) => {
     return (
-      <li className="py-3 px-4">
+      <div>
         <a
           href={href}
           className="font-medium text-sm text-navWords font-['Inter'] tracking-[0.5px] desktop:font-bold desktop:text-lg"
         >
           {title}
         </a>
-      </li>
+      </div>
     );
   };
 
@@ -31,19 +28,12 @@ function Footer() {
       className="bg-gray-100 px-[35px] py-[62.5px] desktop:px-[120px] desktop:py-[136px]"
       id="foot"
     >
-      <div className="flex flex-col desktop:flex-row justify-center items-center gap-6">
+      <div className="flex flex-col desktop:flex-row justify-center desktop:justify-between items-center gap-6">
         <div>Weavers Logo</div>
-        <div className="flex flex-row w-full justify-center gap-20 desktop:justify-end desktop:gap-10">
-          <ul className="flex flex-col items-start justify-between">
-            {firstCol.map((item) => (
-              <FootItem key={item.href} {...item} />
-            ))}
-          </ul>
-          <ul className="flex flex-col iterms-start">
-            {secondCol.map((item) => (
-              <FootItem key={item.href} {...item} />
-            ))}
-          </ul>
+        <div className="grid grid-cols-2 gap-x-44 gap-y-5">
+          {footitems.map((item) => (
+            <FootItem key={item.href} {...item} />
+          ))}
         </div>
       </div>
     </footer>
